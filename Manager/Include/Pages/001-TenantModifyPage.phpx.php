928fe4f73f5680c8ae7cc0fe90695c551b4c4a6d
alcexhim <alcexhim@gmail.com>
2015-03-19 23:17:50 -0400

    Deleted TenantCreatePage and merged with TenantModifyPage

--- Manager/Include/Pages/001-TenantModifyPage.phpx.php
+++ Manager/Include/Pages/001-TenantModifyPage.phpx.php
@@ -13,46 +13,55 @@
 		public function OnInitializing(CancelEventArgs $e)
 		{
 			$page = $e->RenderingPage;
-			$tenant = Tenant::GetByID($page->GetPathVariableValue("tenantID"));
+			return true;
 			
-			$lv = $page->GetControlByID("lvCustomProperties");
-			$properties = $tenant->GetProperties();
-			foreach ($properties as $property)
+			$tenant = null;
+			if ($page->HasPathVariable("tenantID"))
 			{
-				$lvi = new ListViewItem(array
-				(
-					new ListViewItemColumn("lvcPropertyName", $property->Name),
-					new ListViewItemColumn("lvcPropertyDescription", $property->Description),
-					new ListViewItemColumn("lvcPropertyValue", null, function()
-					{
-						$property->RenderEditor($tenant->GetPropertyValue($property));
-					})
-				));
-				$lv->Items[] = $lvi;
+				$tenant = Tenant::GetByID($page->GetPathVariableValue("tenantID"));
 			}
 			
-			$lv = $page->GetControlByID("lvEnabledModules");
-			$modules = Module::Get(null, $this->Tenant);
-			foreach ($modules as $module)
+			if ($tenant != null)
 			{
-				$lvi = new ListViewItem(array
-				(
-					new ListViewItemColumn("lvcModule", $module->Name, "<a href=\"" . System::ExpandRelativePath("~/tenant/modify/" . $tenant->URL . "/modules/" . $module->ID) . "\">" . $module->Title . "</a>"),
-					new ListViewItemColumn("lvcDescription", $module->Description)
-				));
-				$lvModules->Items[] = $lvi;
-			}
-			
-			$lv = $page->GetControlByID("lvGlobalObjects");
-			$objects = TenantObject::Get(null, $this->Tenant);
-			foreach ($objects as $object)
-			{
-				$lvi = new ListViewItem(array
-				(
-					new ListViewItemColumn("lvcObject", $object->Name, "<a href=\"" . System::ExpandRelativePath("~/tenant/manage/" . $tenant->URL . "/objects/" . $object->ID) . "\">" . $object->Name . "</a>"),
-					new ListViewItemColumn("lvcDescription", $object->Description),
-					new ListViewItemColumn("lvcInstances", $object->CountInstances(), "<a href=\"" . System::ExpandRelativePath("~/tenant/manage/" . $tenant->URL . "/objects/" . $object->ID . "/instances") . "\">" . $object->CountInstances() . "</a>")
-				));
+				$lv = $page->GetControlByID("lvCustomProperties");
+				$properties = $tenant->GetProperties();
+				foreach ($properties as $property)
+				{
+					$lvi = new ListViewItem(array
+					(
+						new ListViewItemColumn("lvcPropertyName", $property->Name),
+						new ListViewItemColumn("lvcPropertyDescription", $property->Description),
+						new ListViewItemColumn("lvcPropertyValue", null, function()
+						{
+							$property->RenderEditor($tenant->GetPropertyValue($property));
+						})
+					));
+					$lv->Items[] = $lvi;
+				}
+				
+				$lv = $page->GetControlByID("lvEnabledModules");
+				$modules = Module::Get(null, $this->Tenant);
+				foreach ($modules as $module)
+				{
+					$lvi = new ListViewItem(array
+					(
+						new ListViewItemColumn("lvcModule", $module->Name, "<a href=\"" . System::ExpandRelativePath("~/tenant/modify/" . $tenant->URL . "/modules/" . $module->ID) . "\">" . $module->Title . "</a>"),
+						new ListViewItemColumn("lvcDescription", $module->Description)
+					));
+					$lvModules->Items[] = $lvi;
+				}
+				
+				$lv = $page->GetControlByID("lvGlobalObjects");
+				$objects = TenantObject::Get(null, $this->Tenant);
+				foreach ($objects as $object)
+				{
+					$lvi = new ListViewItem(array
+					(
+						new ListViewItemColumn("lvcObject", $object->Name, "<a href=\"" . System::ExpandRelativePath("~/tenant/manage/" . $tenant->URL . "/objects/" . $object->ID) . "\">" . $object->Name . "</a>"),
+						new ListViewItemColumn("lvcDescription", $object->Description),
+						new ListViewItemColumn("lvcInstances", $object->CountInstances(), "<a href=\"" . System::ExpandRelativePath("~/tenant/manage/" . $tenant->URL . "/objects/" . $object->ID . "/instances") . "\">" . $object->CountInstances() . "</a>")
+					));
+				}
 			}
 		}
 	}
