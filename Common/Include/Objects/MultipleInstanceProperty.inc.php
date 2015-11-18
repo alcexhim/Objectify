38ad9b7e6be16851f435dfaa02fc65afd8192d36
alcexhim <alcexhim@gmail.com>
2015-11-18 14:09:17 -0500

    Provide meaningful messages when assigning wrong instance to object

--- Common/Include/Objects/MultipleInstanceProperty.inc.php
+++ Common/Include/Objects/MultipleInstanceProperty.inc.php
@@ -18,10 +18,34 @@
 		public function AddInstance($value)
 		{
 			if ($value == null) return false;
+			
+			$objIDsAllowed = array();
 			foreach ($this->ValidObjects as $obj)
 			{
-				if ($obj->ID != $value->ParentObject->ID) return false;
+				if ($obj->ID != $value->ParentObject->ID)
+				{
+					$objIDsAllowed[] = $obj->ID;
+				}
 			}
+			
+			$count = count($objIDsAllowed);
+			if ($count > 0)
+			{
+				$objIDsAllowedStr = "";
+				for ($i = 0; $i < $count; $i++)
+				{
+					$objIDsAllowedStr .= $objIDsAllowed[$i];
+					if ($i < $count - 1) $objIDsAllowedStr .= ", ";
+				}
+				Objectify::Log("Attempted to assign an instance to a property which doesn't accept instances of this object", array
+				(
+					"Instance ID" => $value->ID,
+					"Instance Object ID" => $value->ParentObject->ID,
+					"Allowed Object IDs" => $objIDsAllowedStr
+				));
+				return false;
+			}
+			
 			$this->mvarInstances[] = $value;
 			return true;
 		}
