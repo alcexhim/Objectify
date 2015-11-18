6e79ae100834cadb5ae2395a79c7597180102205
alcexhim <alcexhim@gmail.com>
2015-11-18 15:42:55 -0500

    Should eventually be replaced with a Report 'View Objects' or similar

--- Manager/Include/Pages/000-ObjectsPage.phpx.php
+++ Manager/Include/Pages/000-ObjectsPage.phpx.php
@@ -4,15 +4,25 @@
 
 	use Phast\CancelEventArgs;
 	use Phast\Parser\PhastPage;
+	
+	use Phast\WebControls\ListView;
+	use Phast\WebControls\ListViewColumn;
+	use Phast\WebControls\ListViewItem;
+	use Phast\WebControls\ListViewItemColumn;
+	
 	use Objectify\Objects\TenantObject;
 	
 	class ObjectModifyPage extends PhastPage
 	{
+		/**
+		 * The object that is being modified by this ObjectModifyPage.
+		 * @var TenantObject
+		 */
 		public $CurrentObject;
 		
 		public function OnInitializing(CancelEventArgs $e)
 		{
-			$this->CurrentObject = TenantObject::GetByName($e->RenderingPage->GetPathVariableValue("objectID"));
+			$this->CurrentObject = TenantObject::GetByID($e->RenderingPage->GetPathVariableValue("objectID"));
 			
 			$tbsTabs = $e->RenderingPage->GetControlByID("tbsTabs");
 			$tabGeneralInformation = $tbsTabs->GetTabByID("tabGeneralInformation");
@@ -21,6 +31,34 @@
 			if ($this->CurrentObject != null)
 			{
 				$fvGeneralInformation->GetItemByID("txtObjectName")->Value = $this->CurrentObject->Name;
+			}
+			
+			$tabProperties = $tbsTabs->GetTabByID("tabProperties");
+			$lvStaticProperties = $tabProperties->GetControlByID("lvStaticProperties");
+			$lvInstanceProperties = $tabProperties->GetControlByID("lvInstanceProperties");
+			
+			$props = $this->CurrentObject->GetProperties();
+			foreach ($props as $prop)
+			{
+				$lvi = new ListViewItem(array
+				(
+					new ListViewItemColumn("lvcProperty", $prop->Name),
+					new ListViewItemColumn("lvcDataType", $prop->DataType->Name),
+					new ListViewItemColumn("lvcDefaultValue", $prop->DefaultValue)
+				));
+				$lvStaticProperties->Items[] = $lvi;
+			}
+			
+			$props = $this->CurrentObject->GetInstanceProperties();
+			foreach ($props as $prop)
+			{
+				$lvi = new ListViewItem(array
+				(
+					new ListViewItemColumn("lvcProperty", $prop->Name),
+					new ListViewItemColumn("lvcDataType", $prop->DataType->Name),
+					new ListViewItemColumn("lvcDefaultValue", $prop->DefaultValue)
+				));
+				$lvInstanceProperties->Items[] = $lvi;
 			}
 		}
 	}
