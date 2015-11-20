194cbb0fd572184db0bbf324afb211c13b53e4ca
alcexhim <alcexhim@gmail.com>
2015-11-19 23:13:08 -0500

    Preparing to store users and user logins as instances of objects

--- Manager/Include/Pages/000-LoginPage.phpx.php
+++ Manager/Include/Pages/000-LoginPage.phpx.php
@@ -8,8 +8,15 @@
 	
 	use Objectify\Tenant\MasterPages\WebPage;
 	use Objectify\Objects\Tenant;
+	use Objectify\Objects\TenantObject;
+	use Objectify\Objects\TenantObjectInstancePropertyValue;
 	use Objectify\Objects\User;
-	
+	use Objectify\Objects\MultipleInstanceProperty;
+	use Objectify\Objects\SingleInstanceProperty;
+
+	use Phast\RandomStringGenerator;
+	use Phast\RandomStringGeneratorCharacterSets;
+			
 	class LoginPage extends PhastPage
 	{
 		public function OnInitializing(CancelEventArgs $e)
@@ -21,31 +28,38 @@
 					$username = $_POST["user_LoginID"];
 					$password = $_POST["user_Password"];
 					
-					/*
-					 
 					$objUser = TenantObject::GetByName("User");
 					$inst = $objUser->GetInstance(array
 					(
-						new TenantObjectInstancePropertyValue($objUser->GetInstanceProperty("UserName"))
+						new TenantObjectInstancePropertyValue("UserName", $username)
 					));
 					
 					if ($inst != null)
 					{
 						// we have an instance, validate the password
-						$passwordSalt = $inst->GetPropertyValue($objUser->GetInstanceProperty("PasswordSalt"));
-						$passwordHash = $inst->GetPropertyValue($objUser->GetInstanceProperty("PasswordHash"));
+						$passwordSalt = $inst->GetPropertyValue("PasswordSalt");
+						$passwordHash = $inst->GetPropertyValue("PasswordHash");
 						
 						$expectedPasswordHash = hash("sha512", $password . $passwordSalt);
 						
-						trigger_error("password hash: " . $passwordHash);
-						trigger_error("password hash expected: " . $expectedPasswordHash);
 						if ($passwordHash != $expectedPasswordHash)
 						{
 							$inst = null;
 						}
 					}
 					
-					 */
+					$user_LoginToken = RandomStringGenerator::Generate(RandomStringGeneratorCharacterSets::AlphaNumericMixedCase, 32);
+					
+					$objUserLogin = TenantObject::GetByName("UserLogin");
+					$objUserLogin->CreateInstance(array
+					(
+						new TenantObjectInstancePropertyValue("Token", $user_LoginToken),
+						new TenantObjectInstancePropertyValue("SignonTime", (new \DateTime())),
+						new TenantObjectInstancePropertyValue("User", new SingleInstanceProperty($inst, array($objUser))),
+						new TenantObjectInstancePropertyValue("SignoffTime", null),
+						new TenantObjectInstancePropertyValue("DeviceType", new SingleInstanceProperty(null, array($objDeviceType))),
+						new TenantObjectInstancePropertyValue("IPAddress", $_SERVER["REMOTE_ADDR"])
+					));
 					
 					$user = User::GetByCredentials($username, $password);
 					
