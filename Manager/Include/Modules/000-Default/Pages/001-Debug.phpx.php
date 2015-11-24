b912008016bcde0dce9837782e1f2ccf90ae8580
alcexhim <alcexhim@gmail.com>
2015-11-23 23:45:25 -0500

    Fix the debug page so it shows up properly with Phast Pages

--- Manager/Include/Modules/000-Default/Pages/001-Debug.phpx.php
+++ Manager/Include/Modules/000-Default/Pages/001-Debug.phpx.php
@@ -23,11 +23,10 @@
 			{
 				if ($_SERVER["REQUEST_METHOD"] == "POST" && $_POST["action"] == "delete")
 				{
-					$query = "DELETE FROM :tablename WHERE message_ID = :message_ID";
+					$query = "DELETE FROM " . System::GetConfigurationValue("Database.TablePrefix") . "DebugMessages WHERE message_ID = :message_ID";
 					$statement = $pdo->prepare($query);
 					$result = $statement->execute(array
 					(
-						"tablename" => System::GetConfigurationValue("Database.TablePrefix") . "DebugMessages",
 						"message_ID" => $messageID
 					));
 					if ($result !== false)
@@ -38,28 +37,26 @@
 				}
 				else
 				{
-					$query = "SELECT * FROM :tablename WHERE message_ID = :message_ID";
+					$query = "SELECT * FROM " . System::GetConfigurationValue("Database.TablePrefix") . "DebugMessages WHERE message_ID = :message_ID";
 					$statement = $pdo->prepare($query);
 					$statement->execute(array
 					(
-						"tablename" => System::GetConfigurationValue("Database.TablePrefix") . "DebugMessages",
 						"message_ID" => $messageID
 					));
 					$values = $statement->fetch(PDO::FETCH_ASSOC);
 					
-					$this->Page->GetControlByID("divErrorDetails")->EnableRender = true;
-					$this->Page->GetControlByID("lblMessageContent")->Value = $values["message_Content"];
+					$e->RenderingPage->GetControlByID("divErrorDetails")->EnableRender = true;
+					$e->RenderingPage->GetControlByID("lblMessageContent")->Value = $values["message_Content"];
 					
-					$query1 = "SELECT * FROM :tablename WHERE mp_MessageID = :mp_MessageID";
+					$query1 = "SELECT * FROM " . System::GetConfigurationValue("Database.TablePrefix") . "DebugMessageParameters WHERE mp_MessageID = :mp_MessageID";
 					$statement1 = $pdo->prepare($query1);
 					$statement1->execute(array
 					(
-						"tablename" => System::GetConfigurationValue("Database.TablePrefix") . "DebugMessageParameters",
 						"mp_MessageID" => $values["message_ID"]
 					));
 					$count1 = $statement1->rowCount();
 					
-					$lvParameters = $this->Page->GetControlByID("lvParameters");
+					$lvParameters = $e->RenderingPage->GetControlByID("lvParameters");
 					for ($j = 0; $j < $count1; $j++)
 					{
 						$values1 = $statement1->fetch(PDO::FETCH_ASSOC);
@@ -70,23 +67,22 @@
 						));
 					}
 					
-					$query1 = "SELECT * FROM :tablename WHERE bt_MessageID = :bt_MessageID";
+					$query1 = "SELECT * FROM " . System::$Configuration["Database.TablePrefix"] . "DebugMessageBacktraces WHERE bt_MessageID = :bt_MessageID";
 					$statement1 = $pdo->prepare($query1);
 					$statement1->execute(array
 					(
-						":tablename" => System::$Configuration["Database.TablePrefix"] . "DebugMessageBacktraces",
 						":bt_MessageID" => $values["message_ID"]
 					));
 					$count1 = $statement1->rowCount();
 
-					$lvBacktrace = $this->Page->GetControlByID("lvBacktrace");
+					$lvBacktrace = $e->RenderingPage->GetControlByID("lvBacktrace");
 					for ($j = 0; $j < $count1; $j++)
 					{
 						$values1 = $statement1->fetch(PDO::FETCH_ASSOC);
 						$lvBacktrace->Items[] = new ListViewItem(array
 						(
-							new ListViewItemColumn("lvcFileName", "bt_FileName"),
-							new ListViewItemColumn("lvcLineNumber", "bt_LineNumber")
+							new ListViewItemColumn("lvcFileName", $values1["bt_FileName"]),
+							new ListViewItemColumn("lvcLineNumber", $values1["bt_LineNumber"])
 						));
 					}
 				}
@@ -109,16 +105,13 @@
 				}
 				else
 				{
-					$this->Page->GetControlByID("divErrorList")->EnableRender = true;
+					$e->RenderingPage->GetControlByID("divErrorList")->EnableRender = true;
 					
-					$lvMessages = $this->Page->GetControlByID("lvMessages");
+					$lvMessages = $e->RenderingPage->GetControlByID("lvMessages");
 					
-					$query = "SELECT * FROM :tablename";
+					$query = "SELECT * FROM " . System::GetConfigurationValue("Database.TablePrefix") . "DebugMessages";
 					$statement = $pdo->prepare($query);
-					$statement->execute(array
-					(
-						":tablename" => System::$Configuration["Database.TablePrefix"] . "DebugMessages"
-					));
+					$statement->execute();
 					$count = $statement->rowCount();
 					for ($i = 0; $i < $count; $i++)
 					{
@@ -126,6 +119,7 @@
 						
 						$lvi = new ListViewItem();
 						
+						/*
 						$tenant = Tenant::GetByID($values["message_TenantID"]);
 						if ($tenant != null)
 						{
@@ -135,6 +129,7 @@
 						{
 							$lvi->Columns[] = new ListViewItemColumn("lvcTenant", "");
 						}
+						*/
 						
 						switch ($values["message_SeverityID"])
 						{
