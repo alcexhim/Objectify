19cecc95c65fe536ef1d6b1c49c77c4ad026d9a8
alcexhim <alcexhim@gmail.com>
2015-12-18 01:04:40 -0500

    Enhancements to Object browse page

--- Manager/Include/Pages/000-ObjectsPage.phpx.php
+++ Manager/Include/Pages/000-ObjectsPage.phpx.php
@@ -14,6 +14,7 @@
 	use Objectify\Objects\TenantObjectInstance;
 	
 	use Objectify\WebControls\InstanceDisplayWidget;
+	use Objectify\WebControls\ObjectDisplayWidget;
 	
 	class ObjectModifyPage extends PhastPage
 	{
@@ -45,8 +46,46 @@
 			{				
 				$lvi = new ListViewItem(array
 				(
-					new ListViewItemColumn("lvcProperty", $prop->Name),
+					new ListViewItemColumn("lvcProperty", function($sender)
+					{
+						switch ($sender->ExtraData->DataType->Name)
+						{
+							case "SingleInstance":
+							{
+								echo ("<i class=\"fa fa-cube fa-fw\"></i>");
+								break;
+							}
+							case "MultipleInstance":
+							{
+								echo ("<i class=\"fa fa-cubes fa-fw\"></i>");
+								break;
+							}
+							case "Text":
+							{
+								echo ("<i class=\"fa fa-file-text-o fa-fw\"></i>");
+								break;
+							}
+						}
+						echo ($sender->ExtraData->Name);
+					}, null, $prop),
 					new ListViewItemColumn("lvcDataType", $prop->DataType->Name),
+					new ListViewItemColumn("lvcValidObjects", function($sender)
+					{
+						if (is_object($sender->ExtraData))
+						{
+							if (get_class($sender->ExtraData) == "Objectify\\Objects\\MultipleInstanceProperty"
+								|| get_class($sender->ExtraData) == "Objectify\\Objects\\SingleInstanceProperty")
+							{
+								foreach ($sender->ExtraData->ValidObjects as $validObject)
+								{
+									$iv = new ObjectDisplayWidget();
+									$iv->CurrentObject = $validObject;
+									$iv->Render();
+									echo ("<br />");
+								}
+							}
+						}
+					}, null, $this->CurrentObject->GetPropertyValue($prop)),
 					new ListViewItemColumn("lvcValue", function($sender)
 					{
 						if (is_object($sender->ExtraData))
@@ -84,8 +123,49 @@
 			{
 				$lvi = new ListViewItem(array
 				(
-					new ListViewItemColumn("lvcProperty", $prop->Name),
-					new ListViewItemColumn("lvcDataType", $prop->DataType->Name),
+					new ListViewItemColumn("lvcProperty", function($sender)
+					{
+						switch ($sender->ExtraData->DataType->Name)
+						{
+							case "SingleInstance":
+							{
+								echo ("<i class=\"fa fa-cube fa-fw\"></i>");
+								break;
+							}
+							case "MultipleInstance":
+							{
+								echo ("<i class=\"fa fa-cubes fa-fw\"></i>");
+								break;
+							}
+							case "Text":
+							{
+								echo ("<i class=\"fa fa-file-text-o fa-fw\"></i>");
+								break;
+							}
+						}
+						echo ($sender->ExtraData->Name);
+					}, null, $prop),
+					new ListViewItemColumn("lvcDataType", function($sender)
+					{
+						echo ($sender->ExtraData->DataType->Name);
+					}, null, $prop),
+					new ListViewItemColumn("lvcValidObjects", function($sender)
+					{
+						if (is_object($sender->ExtraData))
+						{
+							if (get_class($sender->ExtraData) == "Objectify\\Objects\\MultipleInstanceProperty"
+								|| get_class($sender->ExtraData) == "Objectify\\Objects\\SingleInstanceProperty")
+							{
+								foreach ($sender->ExtraData->ValidObjects as $validObject)
+								{
+									$iv = new ObjectDisplayWidget();
+									$iv->CurrentObject = $validObject;
+									$iv->Render();
+									echo ("<br />");
+								}
+							}
+						}
+					}, null, $prop->DefaultValue),
 					new ListViewItemColumn("lvcDefaultValue", $prop->DefaultValue)
 				));
 				$lvInstanceProperties->Items[] = $lvi;
