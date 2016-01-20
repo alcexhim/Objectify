4967f9fb304bfc5ffca99ee30ef04c86026cb06f
alcexhim <alcexhim@gmail.com>
2016-01-20 12:37:51 -0500

    Only look at the user's DefaultRedirectURL if a User is logged in

--- Manager/Include/Pages/000-DefaultPage.phpx.php
+++ Manager/Include/Pages/000-DefaultPage.phpx.php
@@ -16,12 +16,15 @@ use Objectify\Objects\User;
 			$instTenant = $objTenant->GetInstances()[0];
 			
 			$instUser = User::GetCurrent();
-			$defaultRedirectURL = $instUser->GetPropertyValue("DefaultRedirectURL", null);
-			if ($defaultRedirectURL != null)
+			if ($instUser != null)
 			{
-				System::Redirect($defaultRedirectURL);
-				$e->Cancel = true;
-				return;
+				$defaultRedirectURL = $instUser->GetPropertyValue("DefaultRedirectURL", null);
+				if ($defaultRedirectURL != null)
+				{
+					System::Redirect($defaultRedirectURL);
+					$e->Cancel = true;
+					return;
+				}
 			}
 			
 			$propDefaultRedirectURL = $instTenant->GetPropertyValue("DefaultRedirectURL");
