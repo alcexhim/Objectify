c9976d91f6ee3af7dc4c938c71e076941b02da06
alcexhim <alcexhim@gmail.com>
2016-01-20 00:29:00 -0500

    Add support for a user-based redirect URL property (not implemented yet)

--- Manager/Include/Pages/000-DefaultPage.phpx.php
+++ Manager/Include/Pages/000-DefaultPage.phpx.php
@@ -6,13 +6,23 @@
 	
 	use Phast\Parser\PhastPage;
 	use Objectify\Objects\TenantObject;
-		
+use Objectify\Objects\User;
+			
 	class DefaultPage extends PhastPage
 	{
 		public function OnInitializing(CancelEventArgs $e)
 		{
 			$objTenant = TenantObject::GetByName("Tenant");
 			$instTenant = $objTenant->GetInstances()[0];
+			
+			$instUser = User::GetCurrent();
+			$defaultRedirectURL = $instUser->GetPropertyValue("DefaultRedirectURL", null);
+			if ($defaultRedirectURL != null)
+			{
+				System::Redirect($defaultRedirectURL);
+				$e->Cancel = true;
+				return;
+			}
 			
 			$propDefaultRedirectURL = $instTenant->GetPropertyValue("DefaultRedirectURL");
 			if ($propDefaultRedirectURL != null)
