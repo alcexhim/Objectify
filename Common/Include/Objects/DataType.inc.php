fdfa95f93765d46884b49aa485246173cfec1c17
alcexhim <alcexhim@gmail.com>
2016-01-20 14:02:27 -0500

    Deprecate unused functionality and conform to current standards

--- Common/Include/Objects/DataType.inc.php
+++ Common/Include/Objects/DataType.inc.php
@@ -28,26 +28,6 @@ class DataType
 		$item->EditorRendererCodeBlob = $values["datatype_EditorRendererCodeBlob"];
 		return $item;
 	}
-	public static function Get($max = null)
-	{
-		global $MySQL;
-		$query = "SELECT * FROM " . System::$Configuration["Database.TablePrefix"] . "DataTypes";
-		if (is_numeric($max))
-		{
-			$query .= " LIMIT " . $max;
-		}
-		$result = $MySQL->query($query);
-		$retval = array ();
-		if ($result === false) return $retval;
-		
-		$count = $result->num_rows;
-		for($i = 0; $i < $count; $i++)
-		{
-			$values = $result->fetch_assoc();
-			$retval[] = DataType::GetByAssoc($values);
-		}
-		return $retval;
-	}
 	public static function GetByID($id)
 	{
 		if (!is_numeric($id)) return null;
@@ -90,46 +70,6 @@ class DataType
 		
 		$values = $statement->fetch(PDO::FETCH_ASSOC);
 		return DataType::GetByAssoc($values);
-	}
-	public function Update()
-	{
-		global $MySQL;
-		if ($this->ID == null)
-		{
-			$query = "INSERT INTO " . System::$Configuration["Database.TablePrefix"] . "DataTypes (datatype_Name, datatype_Description, datatype_EncoderCodeBlob, datatype_DecoderCodeBlob, datatype_ColumnRendererCodeBlob, datatype_EditorRendererCodeBlob) VALUES (";
-			$query .= "'" . $MySQL->real_escape_string($this->Name) . "', ";
-			$query .= "'" . $MySQL->real_escape_string($this->Description) . "', ";
-			$query .= ($this->EncoderCodeBlob != null ? ("'" . $MySQL->real_escape_string($this->EncoderCodeBlob) . "'") : "NULL") . ", ";
-			$query .= ($this->DecoderCodeBlob != null ? ("'" . $MySQL->real_escape_string($this->DecoderCodeBlob) . "'") : "NULL") . ", ";
-			$query .= ($this->ColumnRendererCodeBlob != null ? ("'" . $MySQL->real_escape_string($this->ColumnRendererCodeBlob) . "'") : "NULL") . ", ";
-			$query .= ($this->EditorRendererCodeBlob != null ? ("'" . $MySQL->real_escape_string($this->EditorRendererCodeBlob) . "'") : "NULL");
-			$query .= ")";
-		}
-		else
-		{
-			$query = "UPDATE " . System::$Configuration["Database.TablePrefix"] . "DataTypes SET ";
-			$query .= "datatype_Name = '" . $MySQL->real_escape_string($this->Name) . "', ";
-			$query .= "datatype_Description = '" . $MySQL->real_escape_string($this->Description) . "', ";
-			$query .= "datatype_EncoderCodeBlob = '" . ($this->EncoderCodeBlob != null ? ("'" . $MySQL->real_escape_string($this->EncoderCodeBlob) . "'") : "NULL") . "', ";
-			$query .= "datatype_DecoderCodeBlob = '" . ($this->DecoderCodeBlob != null ? ("'" . $MySQL->real_escape_string($this->DecoderCodeBlob) . "'") : "NULL") . "', ";
-			$query .= "datatype_ColumnRendererCodeBlob = '" . ($this->ColumnRendererCodeBlob != null ? ("'" . $MySQL->real_escape_string($this->ColumnRendererCodeBlob) . "'") : "NULL") . "', ";
-			$query .= "datatype_EditorRendererCodeBlob = '" . ($this->EditorRendererCodeBlob != null ? ("'" . $MySQL->real_escape_string($this->EditorRendererCodeBlob) . "'") : "NULL") . "'";
-			$query .= " WHERE datatype_ID = " . $this->ID;
-		}
-		
-		$MySQL->query($query);
-		if ($MySQL->errno != 0)
-		{
-			echo ($MySQL->error);
-			die();
-			return false;
-		}
-		
-		if ($this->ID == null)
-		{
-			$this->ID = $MySQL->insert_id;
-		}
-		return true;
 	}
 	public function Encode($value)
 	{
