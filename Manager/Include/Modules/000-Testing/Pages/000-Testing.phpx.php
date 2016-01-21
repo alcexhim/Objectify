ef9c9e8965f4387a7ffe46f039824a357ef881d3
alcexhim <alcexhim@gmail.com>
2016-01-21 11:54:18 -0500

    Time how long it takes to execute various types of code blocks

--- Manager/Include/Modules/000-Testing/Pages/000-Testing.phpx.php
+++ Manager/Include/Modules/000-Testing/Pages/000-Testing.phpx.php
@@ -5,19 +5,56 @@
 	use Phast\CancelEventArgs;
 	
 	use Objectify\Objects\TenantObject;
-	use Objectify\Objects\Objectify;
+	use Objectify\Objects\TenantObjectInstancePropertyValue;
 	
+	use Objectify\Objects\Objectify;
+	use Phast\Utilities\Stopwatch;
+		
 	class TestingPage extends PhastPage
 	{
 		public function OnInitializing(CancelEventArgs $e)
 		{
-			$objESS = TenantObject::GetByName("ServerSideScriptMethod");
-			$instESS = $objESS->GetInstances();
+			header("Content-Type: text/plain");
 			
-			Objectify::ExecuteMethod("GetCurrentUser", array
+			$sw = new Stopwatch();
+			
+			$objMethod = TenantObject::GetByName("Method");
+			$instMethod = $objMethod->GetInstance(array
 			(
-				"MethodName" => "cast_01102"
+				new TenantObjectInstancePropertyValue("Name", "GetLoginTokenForCurrentUser")
 			));
+			
+			echo ("Method name: " . $instMethod->ToString());
+			echo ("\r\n");
+			
+			$sw->start();
+			$result = Objectify::ExecuteMethod("GetLoginTokenForCurrentUser");
+			$sw->stop();
+			
+			if ($result == null)
+			{
+				echo ("Method Return Value is null");
+			}
+			else
+			{
+				echo ("Method Return Value: " . $result);
+			}
+			echo ("\r\n");
+			echo ("Method Time: " . $sw->getElapsedTime());
+			
+			echo ("\r\n");
+			echo ("\r\n");
+			
+			$sw->start();
+			$result = $_SESSION["Authentication.LoginToken"];
+			$sw->stop();
+			
+			echo ("Non-Method Expression: \$_SESSION[\"Authentication.LoginToken\"]");
+			echo ("\r\n");
+			
+			echo ("Non-Method Result: " . $result);
+			echo ("\r\n");
+			echo ("Non-Method Time: " . $sw->getElapsedTime());
 			
 			die();
 		}
