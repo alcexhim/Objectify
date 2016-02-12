626effef6de7c3fc3626d4ba41aedbaa369f3776
alcexhim <alcexhim@gmail.com>
2016-02-12 13:44:12 -0500

    Prevent dreaded infinite loops!!!

--- Common/Include/Objects/TenantObject.inc.php
+++ Common/Include/Objects/TenantObject.inc.php
@@ -142,9 +142,9 @@
 		{
 			$instRel_Class__has_super_Class = TenantObjectInstance::GetByGlobalIdentifier("{100F0308-855D-4EC5-99FA-D8976CA20053}");
 			$instThisClass = TenantObjectInstance::GetByGlobalIdentifier($this->GlobalIdentifier);
-			$rels = Relationship::GetBySourceInstance($instThisClass, $instRel_Class__has_super_Class);
+			$rels = Relationship::GetBySourceInstance($instThisClass, $instRel_Class__has_super_Class, false);
 			$rels = $rels[0];
-
+			
 			$retval = array();
 			
 			if ($rels != null)
