58e38321517faabd14f3ee67555c68e7ec6b8e76
Michael Becker <alcexhim@gmail.com>
2016-07-19 01:51:32 -0400

    We can finally demolish the TenantObjectProperty(Values) tables!
    Apparently, instance properties have always been more popular than
    static properties... which makes them harder to get rid of :(

--- Manager/Include/Modules/001-Setup/Tables/001-TenantObjects.inc.php
+++ Manager/Include/Modules/001-Setup/Tables/001-TenantObjects.inc.php
@@ -23,48 +23,4 @@
 		new TableForeignKey("TenantID", new TableForeignKeyColumn($tblTenants, "ID"))
 	);
 	$tables[] = $tblTenantObjects;
-	
-	// Available static properties for the objects.
-	$tblTenantObjectProperties = new Table("TenantObjectProperties", "property_", array
-	(
-		// 			$name, $dataType, $size, $value, $allowNull, $primaryKey, $autoIncrement
-		new Column("ID", "INT", null, null, false, true, true),
-		new Column("TenantID", "INT", null, null, false),
-		new Column("ObjectID", "INT", null, null, false),
-		new Column("Name", "VARCHAR", 256, null, true),
-		new Column("DataTypeID", "INT", null, null, true),
-		new Column("DefaultValue", "LONGTEXT", null, null, true),
-		new Column("IsRequired", "INT", null, 0, false),
-		new Column("ColumnVisible", "INT", null, 0, false)
-	));
-	$tblTenantObjectProperties->ForeignKeys = array
-	(
-		new TableForeignKey("TenantID", new TableForeignKeyColumn($tblTenants, "ID")),
-		new TableForeignKey("ObjectID", new TableForeignKeyColumn($tblTenantObjects, "ID")),
-		new TableForeignKey("DataTypeID", new TableForeignKeyColumn($tblDataTypes, "ID"))
-	);
-	$tables[] = $tblTenantObjectProperties;
-	
-	// Values for static properties of objects.
-	$tblTenantObjectPropertyValues = new Table("TenantObjectPropertyValues", "propval_", array
-	(
-		// 			$name, $dataType, $size, $value, $allowNull, $primaryKey, $autoIncrement
-		new Column("TenantID", "INT", null, null, false),
-		new Column("ObjectID", "INT", null, null, false),
-		new Column("PropertyID", "INT", null, null, false),
-		new Column("Value", "LONGTEXT", null, null, true)
-	));
-	$tblTenantObjectPropertyValues->PrimaryKey = new TableKey(array
-	(
-		new TableKeyColumn("TenantID"),
-		new TableKeyColumn("ObjectID"),
-		new TableKeyColumn("PropertyID")
-	));
-	$tblTenantObjectPropertyValues->ForeignKeys = array
-	(
-		new TableForeignKey("TenantID", new TableForeignKeyColumn($tblTenants, "ID")),
-		new TableForeignKey("PropertyID", new TableForeignKeyColumn($tblTenantObjectProperties, "ID")),
-		new TableForeignKey("ObjectID", new TableForeignKeyColumn($tblTenantObjects, "ID"))
-	);
-	$tables[] = $tblTenantObjectPropertyValues;
 ?>
