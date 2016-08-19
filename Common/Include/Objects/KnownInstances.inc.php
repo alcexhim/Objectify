c8261f093bd41eb3a798ae233dcf0233d083f275
Michael Becker <alcexhim@gmail.com>
2016-08-18 23:09:08 -0400

    Keep track of certain well-known, commonly-used instances

---
+++ Common/Include/Objects/KnownInstances.inc.php
@@ -0,0 +1,13 @@
+<?php
+	namespace Objectify\Objects;
+	
+	class KnownInstances
+	{
+		const GUID___Boolean_Attribute___True = "{9F6F4296-2FCD-4B7E-AB09-D25F47D0C54C}";
+		public static function get___Boolean_Attribute___True()
+		{
+			return Instance::GetByGlobalIdentifier(KnownInstances::GUID___Boolean_Attribute___True);
+		}
+	}
+	
+?>
