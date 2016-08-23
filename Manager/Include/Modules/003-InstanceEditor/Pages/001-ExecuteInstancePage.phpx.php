69725f4fc964f6fa99b8e7559c233c49930fa18c
Michael Becker <alcexhim@gmail.com>
2016-08-23 17:45:30 -0400

    Implement even more PageBuilder controls

--- Manager/Include/Modules/003-InstanceEditor/Pages/001-ExecuteInstancePage.phpx.php
+++ Manager/Include/Modules/003-InstanceEditor/Pages/001-ExecuteInstancePage.phpx.php
@@ -4,33 +4,137 @@
 	use Phast\Parser\PhastPage;
 	use Phast\CancelEventArgs;
 	use Phast\System;
-
-	use Phast\WebControls\ListViewColumn;
+	
+	use Phast\WebControls\Disclosure;
 	use Phast\WebControls\FormViewItemText;
 	use Phast\WebControls\FormViewItemChoice;
 	use Phast\WebControls\FormViewItemChoiceValue;
+	use Phast\WebControls\ListView;
+	use Phast\WebControls\ListViewColumn;
+	use Phast\WebControls\ListViewItem;
+	use Phast\WebControls\ListViewItemColumn;
+	use Phast\WebControls\Menu;
+	use Phast\WebControls\MenuItemHeader;
+	use Phast\WebControls\MenuItemCommand;
+	use Phast\WebControls\TabPage;
+	use Phast\WebControls\TabContainer;
+
+	use Phast\HTMLControl;
+	use Phast\HTMLControls\HTMLControlTable;
 	
+	use Phast\WebControlAttribute;
+	use Phast\WebStyleSheetRule;
+	
+	use Objectify\Objects\Objectify;
 	use Objectify\Objects\Instance;
 	use Objectify\Objects\KnownAttributes;
 	use Objectify\Objects\KnownRelationships;
 	use Objectify\Objects\TenantObject;
 	
+	use Objectify\WebControls\InstanceDisplayWidget;
 	use Objectify\WebControls\InstanceListView;
 	use Objectify\WebControls\RelationshipListView;
-use Phast\WebControls\ListView;
-use Phast\HTMLControls\HTMLControlTable;
-use Phast\HTMLControl;
-use Phast\WebStyleSheetRule;
-use Phast\WebControls\Menu;
-use Phast\WebControls\MenuItemHeader;
-use Phast\WebControls\MenuItemCommand;
-use Objectify\Objects\Objectify;
-use Phast\WebControls\ListViewItem;
-use Phast\WebControls\ListViewItemColumn;
-use Objectify\WebControls\InstanceDisplayWidget;
-												
+	
 	class ExecuteInstancePage extends PhastPage
 	{
+		private function CreateControlFromPageComponent($json, $instPageComponent)
+		{
+			$ctl = null;
+			if ($instPageComponent->ParentObject->Name == "RelationshipEditorPageComponent")
+			{
+				$relHasTargetRelationship = $instPageComponent->GetRelationship(KnownRelationships::get___Relationship_Editor_Page_Component__has_target__Relationship());
+				if ($relHasTargetRelationship != null)
+				{
+					$instTargetRelationship = $relHasTargetRelationship->GetDestinationInstance();
+			
+					$lv = new RelationshipListView();
+					$lv->EnableAddRemoveRows = true;
+					$lv->Instance = Instance::GetByGlobalIdentifier($json->Parameters[0]->Value[0]);
+					$lv->Relationship = $instTargetRelationship;
+			
+					$rel_has_Report_Field = $instPageComponent->GetRelationship(KnownRelationships::get___Relationship_Editor_Page_Component__has__Report_Field());
+					if ($rel_has_Report_Field != null)
+					{
+						$instReportFields = $rel_has_Report_Field->GetDestinationInstances();
+						$lv->ReportFields = $instReportFields;
+					}
+			
+					$ctl = $lv;
+				}
+			}
+			else if ($instPageComponent->ParentObject->Name == "TabContainerPageComponent")
+			{
+				$relHasTabContainerTab = $instPageComponent->GetRelationship(KnownRelationships::get___Tab_Container_Page_Component__has__Tab_Container_Tab());
+				if ($relHasTabContainerTab != null)
+				{
+					$tabContainer = new TabContainer();
+					$tabContainer->ID = "TabContainer_" . $instPageComponent->GetInstanceID();
+			
+					$instTabContainerTabs = $relHasTabContainerTab->GetDestinationInstances();
+					foreach ($instTabContainerTabs as $instTabContainerTab)
+					{
+						$tab = new TabPage();
+						$tab->ID = $tabContainer->ID . "_Tab_" . $instTabContainerTab->GetInstanceID();
+							
+						$relTitle = $instTabContainerTab->GetRelationship(KnownRelationships::get___Tab_Container_Tab__has_title__Translatable_Text_Constant());
+						if ($relTitle != null)
+						{
+							$instTitle = $relTitle->GetDestinationInstance();
+							$tab->Title = $instTitle->ToString();
+						}
+						
+						$relComponents = $instTabContainerTab->GetRelationship(KnownRelationships::get___Container_Page_Component__has__Page_Component());
+						if ($relComponents != null)
+						{
+							$instComponents = $relComponents->GetDestinationInstances();
+							foreach ($instComponents as $instComponent)
+							{
+								$ctl1 = $this->CreateControlFromPageComponent($json, $instComponent);
+								if ($ctl1 != null) $tab->Controls[] = $ctl1;
+							}
+						}
+						$tabContainer->TabPages[] = $tab;
+					}
+					
+					if (count($tabContainer->TabPages) > 0)
+					{
+						$tabContainer->SelectedTab = $tabContainer->TabPages[0];
+					}
+					$ctl = $tabContainer;
+				}
+			}
+			else if ($instPageComponent->ParentObject->Name == "AccordionPageComponent")
+			{
+				$accordion = new Disclosure();
+				$accordion->ID = "Accordion_" . $instPageComponent->GetInstanceID();
+					
+				$relTitle = $instPageComponent->GetRelationship(KnownRelationships::get___Accordion_Page_Component__has_title__Translatable_Text_Constant());
+				if ($relTitle != null)
+				{
+					$instTitle = $relTitle->GetDestinationInstance();
+					$accordion->Title = $instTitle->ToString();
+				}
+	
+				$relComponents = $instPageComponent->GetRelationship(KnownRelationships::get___Container_Page_Component__has__Page_Component());
+				if ($relComponents != null)
+				{
+					$instComponents = $relComponents->GetDestinationInstances();
+					foreach ($instComponents as $instComponent)
+					{
+						$ctl1 = $this->CreateControlFromPageComponent($json, $instComponent);
+						if ($ctl1 != null) $accordion->Controls[] = $ctl1;
+					}
+				}
+				$ctl = $accordion;
+			}
+			if ($ctl != null)
+			{
+				$ctl->ClassList[] = "Mocha-Instance";
+				$ctl->Attributes[] = new WebControlAttribute("data-instance-id", $instPageComponent->GetInstanceID());
+			}
+			return $ctl;
+		}
+		
 		public function OnInitializing(CancelEventArgs $e)
 		{
 			$iid = $this->Page->GetPathVariableValue("instanceID");
@@ -41,7 +145,7 @@ use Objectify\WebControls\InstanceDisplayWidget;
 			
 			$idwObjectTitleHeading = $e->RenderingPage->GetControlByID("idwObjectTitleHeading");
 			$idwObjectTitleHeading->CurrentInstance = $inst;
-			$idwObjectTitleHeading->ShowURL = false;
+			// $idwObjectTitleHeading->ShowURL = false;
 			
 			$fvPrompts = $e->RenderingPage->GetControlByID("fvPrompts");
 			if ($paramstr != null)
@@ -53,11 +157,16 @@ use Objectify\WebControls\InstanceDisplayWidget;
 				
 				$layerContent = $e->RenderingPage->GetControlByID("layerContent");
 				
-				$lv = new RelationshipListView();
-				$lv->EnableAddRemoveRows = true;
-				$lv->Instance = Instance::GetByGlobalIdentifier($json->Parameters[0]->Value[0]);
-				$lv->Relationship = Instance::GetByGlobalIdentifier("F9B60C00FF1D438FAC746EDFA8DD7324");
-				$layerContent->Controls[] = $lv;
+				$relPageComponent = $inst->GetRelationship(KnownRelationships::get___UI_Task__has__Page_Component());
+				if ($relPageComponent != null)
+				{
+					$instsPageComponent = $relPageComponent->GetDestinationInstances();
+					foreach ($instsPageComponent as $instPageComponent)
+					{
+						$ctl = $this->CreateControlFromPageComponent($json, $instPageComponent);
+						if ($ctl != null) $layerContent->Controls[] = $ctl;
+					}
+				}
 			}
 			else
 			{
@@ -207,31 +316,55 @@ use Objectify\WebControls\InstanceDisplayWidget;
 				$td->StyleRules[] = new WebStyleSheetRule("vertical-align", "top");
 				
 				$lvReport = new ListView();
-				$rel = $inst->GetRelationship(KnownRelationships::get___Report__has__Report_Field());
+				$rel = $inst->GetRelationship(KnownRelationships::get___Report__has__Report_Column());
 				if ($rel != null)
 				{
-					$instsReportField = $rel->GetDestinationInstances();
-					foreach ($instsReportField as $instReportField)
+					$instsReportColumn = $rel->GetDestinationInstances();
+					foreach ($instsReportColumn as $instReportColumn)
 					{
-						$title = $instReportField->ToString();
-						if ($instReportField->ParentObject->Name == "PrimaryObjectReportField")
+						$relReportField = $instReportColumn->GetRelationship(KnownRelationships::get___Report_Column__has__Report_Field());
+						if ($relReportField != null)
 						{
-							$title = $instsRow[0]->ParentObject->Name;
+							$instReportField = $relReportField->GetDestinationInstance();
+							$title = $instReportField->ToString();
+							if ($instReportField->ParentObject->Name == "PrimaryObjectReportField")
+							{
+								$title = $instsRow[0]->ParentObject->Name;
+							}
+							$lvReport->Columns[] = new ListViewColumn("ch" . $instReportField->ID, $title);
 						}
-						$lvReport->Columns[] = new ListViewColumn("ch" . $instReportField->ID, $title);
 					}
 
 					foreach ($instsRow as $instRow)
 					{
 						$lvi = new ListViewItem();
-						$countReportField = count($instsReportField);
-						for ($i = 0; $i < $countReportField; $i++)
+						$countReportColumn= count($instsReportColumn);
+						for ($i = 0; $i < $countReportColumn; $i++)
 						{
-							$instReportField = $instsReportField[$i];
+							$instReportColumn = $instsReportColumn[$i];
+							
+							$relReportField = $instReportColumn->GetRelationship(KnownRelationships::get___Report_Column__has__Report_Field());
+							$instReportField = $relReportField->GetDestinationInstance();
+							
 							$lvi->Columns[] = new ListViewItemColumn($lvReport->Columns[$i]->ID, function($sender)
 							{
 								$instRow = $sender->ExtraData[0];
 								$instReportField = $sender->ExtraData[1];
+								$instReportColumn = $sender->ExtraData[2];
+								
+								$displayAsCount = false;
+								$relHas_Report_Column_Option = $instReportColumn->GetRelationship(KnownRelationships::get___Report_Column__has__Report_Column_Option());
+								if ($relHas_Report_Column_Option != null)
+								{
+									$instsHas_Report_Column_Option = $relHas_Report_Column_Option->GetDestinationInstances();
+									foreach ($instsHas_Report_Column_Option as $instRCO)
+									{
+										if ($instRCO->GlobalIdentifier == "5C9B4C79995B4E6A81C039C174BF9F6D")
+										{
+											$displayAsCount = true;
+										}
+									}
+								}
 								
 								$value = Objectify::GetReportFieldValue($instReportField, $instRow);
 								if (is_object($value))
@@ -246,23 +379,40 @@ use Objectify\WebControls\InstanceDisplayWidget;
 										else
 										{
 										*/
-										$idw = new InstanceDisplayWidget($value);
-										$idw->Render();
+										
+										if ($displayAsCount)
+										{
+											echo ("1");
+										}
+										else
+										{
+											$idw = new InstanceDisplayWidget($value);
+											$idw->Render();
+										}
 									}
 								}
 								else if (is_array($value))
 								{
-									foreach ($value as $val)
+									if ($displayAsCount)
 									{
-										if (get_class($val) == "Objectify\\Objects\\Instance")
+										echo("<a href=\"#\" class=\"InstanceListDropDown\" data-row-instance-id=\"" . $instRow->GetInstanceID() . "\" data-field-instance-id=\"" . $instReportField->GetInstanceID() . "\">");
+										echo (count($value));
+										echo (" <i class=\"fa fa-caret-down\"></i></a>");
+									}
+									else
+									{
+										foreach ($value as $val)
 										{
-											$idw = new InstanceDisplayWidget($val);
-											$idw->Render();
-											echo ("<br />");
-										}
-										else
-										{
-											echo ("<!-- GetReportFieldValue not defined for class `" . get_class($val) . "` -->");
+											if (get_class($val) == "Objectify\\Objects\\Instance")
+											{
+												$idw = new InstanceDisplayWidget($val);
+												$idw->Render();
+												echo ("<br />");
+											}
+											else
+											{
+												echo ("<!-- GetReportFieldValue not defined for class `" . get_class($val) . "` -->");
+											}
 										}
 									}
 								}
@@ -270,7 +420,7 @@ use Objectify\WebControls\InstanceDisplayWidget;
 								{
 									echo ($value);
 								}
-							}, $instRow->ToString(), array($instRow, $instReportField));
+							}, $instRow->ToString(), array($instRow, $instReportField, $instReportColumn));
 						}
 						$lvReport->Items[] = $lvi;
 					}
