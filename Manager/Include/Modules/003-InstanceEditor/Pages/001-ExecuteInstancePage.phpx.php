864612be87df4c910bea11946dc7324e2c89048a
Michael Becker <alcexhim@gmail.com>
2016-09-04 12:20:53 -0400

    Refactor report generation into separate ReportListView control
    This allows us to use ReportPageComponents to insert "sub" reports into
    other areas of the system.

--- Manager/Include/Modules/003-InstanceEditor/Pages/001-ExecuteInstancePage.phpx.php
+++ Manager/Include/Modules/003-InstanceEditor/Pages/001-ExecuteInstancePage.phpx.php
@@ -15,10 +15,6 @@
 	use Phast\WebControls\FormViewItemLabel;
 	use Phast\WebControls\FormViewItemText;
 	
-	use Phast\WebControls\ListView;
-	use Phast\WebControls\ListViewColumn;
-	use Phast\WebControls\ListViewItem;
-	use Phast\WebControls\ListViewItemColumn;
 	use Phast\WebControls\Menu;
 	use Phast\WebControls\MenuItemHeader;
 	use Phast\WebControls\MenuItemCommand;
@@ -38,8 +34,7 @@
 	use Objectify\Objects\TenantObject;
 
 	use Objectify\WebControls\FormViewItemInstance;
-	use Objectify\WebControls\InstanceDisplayWidget;
-	use Objectify\WebControls\InstanceListView;
+	use Objectify\WebControls\ReportListView;
 	use Objectify\WebControls\RelationshipListView;
 	
 	class ExecuteInstancePage extends PhastPage
@@ -385,131 +380,12 @@
 					$tr->Controls[] = $td;
 				}
 				
-				$relDataSource = $inst->GetRelationship(KnownRelationships::get___Report__has__Report_Data_Source());
-				if ($relDataSource == null) return;
-				
-				$instDataSource = $relDataSource->GetDestinationInstance();
-				if ($instDataSource == null) return;
-				
-				$relSourceMethod = $instDataSource->GetRelationship(KnownRelationships::get___Report_Data_Source__has_source__Method());
-				if ($relSourceMethod == null) return;
-				
-				$instSourceMethod = $relSourceMethod->GetDestinationInstance();
-				$instsRow = Objectify::ExecuteMethod($instSourceMethod);
-				
 				$td = new HTMLControl("td");
 				$td->StyleRules[] = new WebStyleSheetRule("vertical-align", "top");
 				
-				$lvReport = new ListView();
-				$rel = $inst->GetRelationship(KnownRelationships::get___Report__has__Report_Column());
-				if ($rel != null)
-				{
-					$instsReportColumn = $rel->GetDestinationInstances();
-					foreach ($instsReportColumn as $instReportColumn)
-					{
-						$relReportField = $instReportColumn->GetRelationship(KnownRelationships::get___Report_Column__has__Report_Field());
-						if ($relReportField != null)
-						{
-							$instReportField = $relReportField->GetDestinationInstance();
-							$title = $instReportField->ToString();
-							if ($instReportField->ParentObject->Name == "PrimaryObjectReportField")
-							{
-								$title = $instsRow[0]->ParentObject->Name;
-							}
-							$lvReport->Columns[] = new ListViewColumn("ch" . $instReportField->ID, $title);
-						}
-					}
-
-					foreach ($instsRow as $instRow)
-					{
-						$lvi = new ListViewItem();
-						$countReportColumn= count($instsReportColumn);
-						for ($i = 0; $i < $countReportColumn; $i++)
-						{
-							$instReportColumn = $instsReportColumn[$i];
-							
-							$relReportField = $instReportColumn->GetRelationship(KnownRelationships::get___Report_Column__has__Report_Field());
-							$instReportField = $relReportField->GetDestinationInstance();
-							
-							$lvi->Columns[] = new ListViewItemColumn($lvReport->Columns[$i]->ID, function($sender)
-							{
-								$instRow = $sender->ExtraData[0];
-								$instReportField = $sender->ExtraData[1];
-								$instReportColumn = $sender->ExtraData[2];
-								
-								$displayAsCount = false;
-								$relHas_Report_Column_Option = $instReportColumn->GetRelationship(KnownRelationships::get___Report_Column__has__Report_Column_Option());
-								if ($relHas_Report_Column_Option != null)
-								{
-									$instsHas_Report_Column_Option = $relHas_Report_Column_Option->GetDestinationInstances();
-									foreach ($instsHas_Report_Column_Option as $instRCO)
-									{
-										if ($instRCO->GlobalIdentifier == "5C9B4C79995B4E6A81C039C174BF9F6D")
-										{
-											$displayAsCount = true;
-										}
-									}
-								}
-								
-								$value = Objectify::GetReportFieldValue($instReportField, $instRow);
-								if (is_object($value))
-								{
-									if (get_class($value) == "Objectify\\Objects\\Instance")
-									{
-										/*
-										if ($value->HasParentObject(KnownObjects::get___Attribute()))
-										{
-											echo($instRow->GetAttributeValue($instAttribute, "(empty)"));
-										}
-										else
-										{
-										*/
-										
-										if ($displayAsCount)
-										{
-											echo ("1");
-										}
-										else
-										{
-											$idw = new InstanceDisplayWidget($value);
-											$idw->Render();
-										}
-									}
-								}
-								else if (is_array($value))
-								{
-									if ($displayAsCount)
-									{
-										echo("<a href=\"#\" class=\"InstanceListDropDown\" data-row-instance-id=\"" . $instRow->GetInstanceID() . "\" data-field-instance-id=\"" . $instReportField->GetInstanceID() . "\">");
-										echo (count($value));
-										echo (" <i class=\"fa fa-caret-down\"></i></a>");
-									}
-									else
-									{
-										foreach ($value as $val)
-										{
-											if (get_class($val) == "Objectify\\Objects\\Instance")
-											{
-												$idw = new InstanceDisplayWidget($val);
-												$idw->Render();
-												echo ("<br />");
-											}
-											else
-											{
-												echo ("<!-- GetReportFieldValue not defined for class `" . get_class($val) . "` -->");
-											}
-										}
-									}
-								}
-								else
-								{
-									echo ($value);
-								}
-							}, $instRow->ToString(), array($instRow, $instReportField, $instReportColumn));
-						}
-						$lvReport->Items[] = $lvi;
-					}
-				}
+				$lvReport = new ReportListView();
+				$lvReport->Report = $inst;
+				
 				$td->Controls[] = $lvReport;
 				
 				$tr->Controls[] = $td;
---
+++ Manager/Include/WebControls/ReportListView.inc.php
@@ -0,0 +1,178 @@
+<?php
+	
+	namespace Objectify\WebControls;
+	
+	use Phast\WebControls\ListView;
+	use Phast\WebControls\ListViewColumn;
+	use Phast\WebControls\ListViewItem;
+	use Phast\WebControls\ListViewItemColumn;
+	
+	use Objectify\Objects\Instance;
+	use Objectify\Objects\KnownRelationships;
+	use Objectify\Objects\Objectify;
+use Phast\HTMLControls\Layer;
+use Phast\WebControls\Button;
+use Phast\WebControls\Menu;
+use Phast\WebControls\MenuItemCommand;
+					
+	class ReportListView extends ListView
+	{
+		/**
+		 * The Instance of the Report to display in this ReportListView.
+		 * @var Instance
+		 */
+		public $Report;
+		
+		protected function RenderContent()
+		{
+			$div = new Layer();
+			$div->ClassList[] = "ListViewControls";
+			
+			$cmdOptions = new Button();
+			$cmdOptions->DropDownRequired = true;
+			$cmdOptions->IconName = "cog";
+			
+			$menuOptions = new Menu();
+			$menuOptions->ClassList[] = "Popup";
+			$menuOptions->ClassList[] = "Visible";
+			$menuOptions->ClassList[] = "Visible-Always";
+			
+			$cmdOptionsDownload = new MenuItemCommand("Download");
+			$cmdOptionsDownload->IconName = "download";
+			$menuOptions->Items[] = $cmdOptionsDownload;
+
+			$cmdOptionsPrint = new MenuItemCommand("Print");
+			$cmdOptionsPrint->IconName = "print";
+			$menuOptions->Items[] = $cmdOptionsPrint;
+			
+			$cmdOptions->DropDownControls[] = $menuOptions;
+			$div->Controls[] = $cmdOptions;
+			
+			$div->Render();
+			
+			$rel = $this->Report->GetRelationship(KnownRelationships::get___Report__has__Report_Column());
+			if ($rel != null)
+			{
+				$relDataSource = $this->Report->GetRelationship(KnownRelationships::get___Report__has__Report_Data_Source());
+				if ($relDataSource == null) return;
+				
+				$instDataSource = $relDataSource->GetDestinationInstance();
+				if ($instDataSource == null) return;
+				
+				$relSourceMethod = $instDataSource->GetRelationship(KnownRelationships::get___Report_Data_Source__has_source__Method());
+				if ($relSourceMethod == null) return;
+				
+				$instSourceMethod = $relSourceMethod->GetDestinationInstance();
+				$instsRow = Objectify::ExecuteMethod($instSourceMethod);
+
+				$instsReportColumn = $rel->GetDestinationInstances();
+				foreach ($instsReportColumn as $instReportColumn)
+				{
+					$relReportField = $instReportColumn->GetRelationship(KnownRelationships::get___Report_Column__has__Report_Field());
+					if ($relReportField != null)
+					{
+						$instReportField = $relReportField->GetDestinationInstance();
+						$title = $instReportField->ToString();
+						if ($instReportField->ParentObject->Name == "PrimaryObjectReportField")
+						{
+							$title = $instsRow[0]->ParentObject->Name;
+						}
+						$this->Columns[] = new ListViewColumn("ch" . $instReportField->ID, $title);
+					}
+				}
+				
+				foreach ($instsRow as $instRow)
+				{
+					$lvi = new ListViewItem();
+					$countReportColumn= count($instsReportColumn);
+					for ($i = 0; $i < $countReportColumn; $i++)
+					{
+						$instReportColumn = $instsReportColumn[$i];
+							
+						$relReportField = $instReportColumn->GetRelationship(KnownRelationships::get___Report_Column__has__Report_Field());
+						$instReportField = $relReportField->GetDestinationInstance();
+							
+						$lvi->Columns[] = new ListViewItemColumn($this->Columns[$i]->ID, function($sender)
+						{
+							$instRow = $sender->ExtraData[0];
+							$instReportField = $sender->ExtraData[1];
+							$instReportColumn = $sender->ExtraData[2];
+			
+							$displayAsCount = false;
+							$relHas_Report_Column_Option = $instReportColumn->GetRelationship(KnownRelationships::get___Report_Column__has__Report_Column_Option());
+							if ($relHas_Report_Column_Option != null)
+							{
+								$instsHas_Report_Column_Option = $relHas_Report_Column_Option->GetDestinationInstances();
+								foreach ($instsHas_Report_Column_Option as $instRCO)
+								{
+									if ($instRCO->GlobalIdentifier == "5C9B4C79995B4E6A81C039C174BF9F6D")
+									{
+										$displayAsCount = true;
+									}
+								}
+							}
+			
+							$value = Objectify::GetReportFieldValue($instReportField, $instRow);
+							if (is_object($value))
+							{
+								if (get_class($value) == "Objectify\\Objects\\Instance")
+								{
+									/*
+									 if ($value->HasParentObject(KnownObjects::get___Attribute()))
+									 {
+									 echo($instRow->GetAttributeValue($instAttribute, "(empty)"));
+									 }
+									 else
+									 {
+									 */
+			
+									if ($displayAsCount)
+									{
+										echo ("1");
+									}
+									else
+									{
+										$idw = new InstanceDisplayWidget($value);
+										$idw->Render();
+									}
+								}
+							}
+							else if (is_array($value))
+							{
+								if ($displayAsCount)
+								{
+									echo("<a href=\"#\" class=\"InstanceListDropDown\" data-row-instance-id=\"" . $instRow->GetInstanceID() . "\" data-field-instance-id=\"" . $instReportField->GetInstanceID() . "\">");
+									echo (count($value));
+									echo (" <i class=\"fa fa-caret-down\"></i></a>");
+								}
+								else
+								{
+									foreach ($value as $val)
+									{
+										if (get_class($val) == "Objectify\\Objects\\Instance")
+										{
+											$idw = new InstanceDisplayWidget($val);
+											$idw->Render();
+											echo ("<br />");
+										}
+										else
+										{
+											echo ("<!-- GetReportFieldValue not defined for class `" . get_class($val) . "` -->");
+										}
+									}
+								}
+							}
+							else
+							{
+								echo ($value);
+							}
+						}, $instRow->ToString(), array($instRow, $instReportField, $instReportColumn));
+					}
+					$this->Items[] = $lvi;
+				}
+			}
+			
+			parent::RenderContent();
+		}
+	}
+?>
